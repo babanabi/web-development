a500b7dbffbd2ca1ec323ac14e457cb591f10893
babanabi <76717223+babanabi@users.noreply.github.com>
2021-08-09 00:41:33 -0400

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,19 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+           'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F',
+           'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+           'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+def password_generator(letter=4,number=2,symbol=2):
+
+    four_letters = random.sample(letters,letter)
+    two_numbers = random.sample(numbers,number)
+    two_symbols = random.sample(symbols,symbol)
+
+    result = four_letters + two_numbers + two_symbols
+    random.shuffle(result)
+    password = ''.join(result)
+    return password
